b8db2acf8f0221a1e91038a46ad454308c5b6c0b
Michael Bissinger <Michael.Bissinger@inb.de>
2019-06-13 12:27:50 +0200

    create int for scores and add toast-message

--- app/src/main/java/example/com/MainActivity.java
+++ app/src/main/java/example/com/MainActivity.java
@@ -8,8 +8,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    // Holding the score
+
+    int score;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        // Set initial score
+        score = 0;
+
+        Toast.makeText(getApplicationContext(), "Application starting!", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
