d29e3fe1537c3c8220cdf8b7225b6104e701b05d
Michael Bissinger <Michael.Bissinger@inb.de>
2019-06-19 12:12:46 +0200

    fixing bug in logic

--- app/src/main/java/example/com/MainActivity.java
+++ app/src/main/java/example/com/MainActivity.java
@@ -121,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
             int scoreDelta = die1 * 100;
             msg = "You rolled a triple " + die1 + "! You score " + scoreDelta + " points!";
             score += scoreDelta;
-        } else if (die1 == die2 || die1 == die3) {
+        } else if (die1 == die2 || die2 == die3) {
             //double-score
             msg = "You rolled doubles for 50 points!";
             score += 50;
